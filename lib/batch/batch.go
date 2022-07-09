2b6151c1913c32b52876605e149523e4639a4c81
aabramovdev <aabramov.dev@gmail.com>
2022-07-09 12:48:43 +0500

    My solution for homework-11

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sort"
 	"time"
 )
 
@@ -13,6 +14,63 @@ func getOne(id int64) user {
 	return user{ID: id}
 }
 
+// func getBatch(n int64, pool int64) (res []user) {
+// 	res = make([]user, n)
+// 	var wg sync.WaitGroup
+// 	var m sync.Mutex
+// 	sem := make(chan struct{}, pool)
+// 	var i int64
+// 	for i = 0; i < n; i++ {
+// 		wg.Add(1)
+// 		sem <- struct{}{}
+// 		go func(j int64) {
+// 			defer wg.Done()
+// 			m.Lock()
+// 			res[j] = getOne(j)
+// 			m.Unlock()
+// time.Sleep(time.Nanosecond * 20)
+// <-sem
+// wg.Done()
+// }(i)
+// }
+// wg.Wait()
+// return
+// }
+
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	jobs := make(chan int64, n)
+	results := make(chan user, n)
+	var w int64
+	for w = 0; w < pool; w++ {
+		go worker(w, jobs, results)
+	}
+	var idx int64
+	for idx = 0; idx < n; idx++ {
+		jobs <- idx
+	}
+	close(jobs)
+
+	return sortUsers(chanToUsers(n, results))
+}
+
+func worker(id int64, jobs <-chan int64, results chan<- user) {
+	for j := range jobs {
+		// fmt.Println("Worker  ID:", id)
+		results <- getOne(j)
+	}
+}
+
+func chanToUsers(n int64, results <-chan user) (users []user) {
+	var idx int64
+	for idx = 0; idx < n; idx++ {
+		users = append(users, <-results)
+	}
+	return
+}
+
+func sortUsers(users []user) []user {
+	sort.Slice(users, func(i, j int) bool {
+		return users[i].ID < users[j].ID
+	})
+	return users
 }
